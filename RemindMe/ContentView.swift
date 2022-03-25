409316a51a18478e3bff1719c454ff888bacf7bf
Ashton Price <apric9@mybce.catholic.edu.au>
2022-03-25 13:37:27 +1000

    Initial Commit

---
+++ RemindMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RemindMe
+//
+//  Created by Ashton Price on 25/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RemindMe/RemindMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindMeApp.swift
+//  RemindMe
+//
+//  Created by Ashton Price on 25/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct RemindMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
